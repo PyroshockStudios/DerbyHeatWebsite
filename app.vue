a873e9691c4cf046bfcea161443fe509b7c796a9
TJ-LOK-TRL <2221481@my.ipleiria.pt>
2025-02-08 17:45:31 +0000

    install and test tailwind/postcss

--- app.vue
+++ app.vue
@@ -1,36 +1,29 @@
 <template>
-    <div>
-      <header>
-        <h1>Meu App Nuxt 3</h1>
-        <nav>
-          <ul>
-            <li><NuxtLink to="/">Home</NuxtLink></li>  <!-- Link para a página inicial -->
-            <li><NuxtLink to="/sobre">Sobre</NuxtLink></li> <!-- Link para outra página -->
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <NuxtPage />
-      </main>
-    </div>
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style scoped>
-  header {
-    background-color: #333;
-    color: white;
-    padding: 1rem;
-  }
-  
-  nav ul {
-    list-style-type: none;
-  }
-  
-  nav li {
-    display: inline;
-    margin-right: 10px;
-  }
-  </style>
+  <div>
+    <header2 />
+    <main>
+      <NuxtPage />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import header2 from '../components/header.vue';
+</script>
+
+<style scoped>
+header {
+  background-color: #333;
+  color: white;
+  padding: 1rem;
+}
+
+nav ul {
+  list-style-type: none;
+}
+
+nav li {
+  display: inline;
+  margin-right: 10px;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
 export default {
   // Also supports server
   target: 'static',
-
-  build: {
-      analyze: true
+  css: [
+    '~/assets/css/tailwind.css',
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   },
-
+  build: {
+    analyze: true,
+  },
   debug: true,
-
   compatibilityDate: '2025-02-01'
 };
