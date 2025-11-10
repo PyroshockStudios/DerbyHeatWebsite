3f6d804620e853761976cf688255f9cfdb906ef1
ZilverBlade <90721817+ZilverBlade@users.noreply.github.com>
2025-11-10 18:34:28 +0100

    redid entire vue environment test

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-// vite.config.ts
-import { defineConfig } from "vite";
+import { fileURLToPath, URL } from 'node:url'
 
-// Don't trigger a rebuild of the Vite server on local content file changes
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    hmr: { path: "/vite-hmr/" },
-    watch: {
-      ignored: ["**/content/**"],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-});
+})
